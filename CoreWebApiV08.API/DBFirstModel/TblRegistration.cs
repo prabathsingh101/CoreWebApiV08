29a1db6870b2fa3bd3e42c61f645e8877dc2185e
DotNetCoreProjects <pappusingh954055@gmail.com>
2024-07-10 23:45:11 +0530

    code pushe

---
+++ CoreWebApiV08.API/DBFirstModel/TblRegistration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreWebApiV08.API.DBFirstModel;
+
+public partial class TblRegistration
+{
+    public int Id { get; set; }
+
+    public int? Registrationno { get; set; }
+
+    public string? Fname { get; set; }
+
+    public string? Lname { get; set; }
+
+    public DateTime? Registrationdate { get; set; }
+
+    public decimal? Registrationfees { get; set; }
+
+    public string? Mobileno { get; set; }
+
+    public DateTime? Createddate { get; set; }
+
+    public DateTime? Updateddate { get; set; }
+
+    public string? Address { get; set; }
+
+    public int? Classid { get; set; }
+
+    public string Fathersname { get; set; } = null!;
+
+    public bool? IsDeleted { get; set; }
+}
---
+++ CoreWebApiV08.API/DBFirstModel/TblStudent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreWebApiV08.API.DBFirstModel;
+
+public partial class TblStudent
+{
+    public int Id { get; set; }
+
+    public int? Registrationno { get; set; }
+
+    public string? Fname { get; set; }
+
+    public string? Lname { get; set; }
+
+    public DateTime? Registrationdate { get; set; }
+
+    public DateTime? Admissiondate { get; set; }
+
+    public decimal? Registrationfees { get; set; }
+
+    public string? Mobileno { get; set; }
+
+    public string? Address { get; set; }
+
+    public int? Classid { get; set; }
+
+    public string Fathersname { get; set; } = null!;
+
+    public DateTime? Createddate { get; set; }
+
+    public DateTime? Updateddate { get; set; }
+
+    public bool? IsDeleted { get; set; }
+}
